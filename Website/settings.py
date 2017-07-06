e17e5b155518cb69c79a56b1631c4d59af70f41e
Sumeet Wilkhu <sumeetwilkhu@Sumeet-MacBook-Air.local>
2017-07-05 21:42:29 -0500

    Adding new changes

--- Website/settings.py
+++ Website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pl-azwm1l#$-#h+hw#75n_ra-lsay4i4#g=k_5^*h!vd^50_p*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['weedgenomics-dev.us-east-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['localhost','weedgenomics-dev.us-east-1.elasticbeanstalk.com']
 
 
 # Application definition
