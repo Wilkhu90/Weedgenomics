61d7c8aad72e2acf6581522ad98c510e0e164c0e
sunitsivaraj <sunitsivaraj@gmail.com>
2017-07-19 20:29:04 -0500

    Updated contact page

--- search/forms.py
+++ search/forms.py
@@ -4,5 +4,7 @@ from django import forms
 # A form where users can contact the website Admin
 class ContactForm(forms.Form):
     name = forms.CharField(required=True, max_length=100, help_text='100 chars max')
+    affiliation = forms.CharField(required=True, max_length=100, help_text='100 chars max')
+    position = forms.CharField(required=True, max_length=100, help_text='100 chars max')
     email = forms.EmailField(required=True)
     comment = forms.CharField(required=True, widget=forms.Textarea)
