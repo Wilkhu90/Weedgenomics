af3ce0d9683c369d91cbf6b47767f9ec3900051b
Sumeet Wilkhu <sumeetwilkhu@Sumeet-MacBook-Air.local>
2017-08-01 13:02:39 -0500

    Adding changes discussed.

--- search/views.py
+++ search/views.py
@@ -197,8 +197,15 @@ def blastn_at_ncbi(request, seq_id):
     hits = []
     for result in results:
         for hit in result.hsps:
-            hits.append({"Hit_exp": hit.expect, "Hit_query": hit.query,
-                         "Hit_match": hit.match, "Hit_sbject": hit.sbjct,
+            changedQuery = re.sub(r"-", "_", hit.query)
+            times = int(math.ceil(len(changedQuery)/60.0))
+            size = 60
+            length = len(hit.match)
+            new_seq = []
+            for i in range(0, times):
+                new_seq.append({"Hit_query": changedQuery[i*size:(i+1)*size], "Hit_match": hit.match[i*size:(i+1)*size], "Hit_sbject": hit.sbjct[i*size:(i+1)*size]})
+
+            hits.append({"Hit_exp": hit.expect, "new_seq": new_seq, "length": length,
                          "description": query_sequence.gene_description, "ID": query_sequence.id, "Hit_id": result.hit_id})
     context = {"hits": hits}
     template = "search/blastn_results.html"
@@ -261,8 +268,15 @@ def blastx_at_ncbi(request, seq_id):
     hits = []
     for result in results:
         for hit in result.hsps:
-            hits.append({"Hit_exp": hit.expect, "Hit_query": hit.query,
-                         "Hit_match": hit.match, "Hit_sbject": hit.sbjct,
+            changedQuery = re.sub(r"-", "_", hit.query)
+            times = int(math.ceil(len(changedQuery)/60.0))
+            size = 60
+            length = len(hit.match)
+            new_seq = []
+            for i in range(0, times):
+                new_seq.append({"Hit_query": changedQuery[i*size:(i+1)*size], "Hit_match": hit.match[i*size:(i+1)*size], "Hit_sbject": hit.sbjct[i*size:(i+1)*size]})
+
+            hits.append({"Hit_exp": hit.expect, "new_seq": new_seq, "length": length,
                          "description": query_sequence.gene_description, "ID": query_sequence.id, "Hit_id": result.hit_id})
     context = {"hits": hits}
 
