ed094973c9307ef7d43cf047a839cd34c559408d
Sumeet Wilkhu <sumeetwilkhu@Sumeet-MacBook-Air.local>
2017-08-01 11:09:23 -0500

    Adding changes discussed.

--- search/views.py
+++ search/views.py
@@ -176,14 +176,12 @@ def blastn_search(request):
                 changedQuery = re.sub(r"-", "_", hit.query)
                 times = int(math.ceil(len(changedQuery)/60.0))
                 size = 60
-                newQuery = []
-                newMatch = []
-                newSubject = []
+                length = len(hit.match)
                 new_seq = []
                 for i in range(0, times):
                     new_seq.append({"Hit_query": changedQuery[i*size:(i+1)*size], "Hit_match": hit.match[i*size:(i+1)*size], "Hit_sbject": hit.sbjct[i*size:(i+1)*size]})
 
-                hits.append({"Hit_exp": hit.expect, "new_seq": new_seq,
+                hits.append({"Hit_exp": hit.expect, "new_seq": new_seq, "length": length,
                 "description": sequence.gene_description, "ID": sequence.id, "Hit_id": result.hit_id})
 
     context = {"hits": hits, "searchQuery": {"species": database_name, "query": name}}
